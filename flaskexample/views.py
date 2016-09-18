54066e6245d8247f58892245ad28cd078c3b965c
Rich Winslow <rwins4@gmail.com>
2016-09-18 02:47:48 -0700

    Added way to get player health to scene segmentation

--- flaskexample/views.py
+++ flaskexample/views.py
@@ -9,6 +9,7 @@ from flask import request
 from flaskexample import app
 
 from PIL import Image, ImageFilter
+import cv2
 import imageio
 import numpy as np
 import pandas as pd
@@ -42,18 +43,13 @@ def hog_img(img):
 def img2features(img):
     return hog_img(img)
 
-def scene_detection():
+def scene_detection(positive_path, negative_path):
     """Train scene detection classifier"""
 
     # Print status
     print('Training scene detection classifier')
 
     # Build training set of classified images
-    # basepath = '/Volumes/Passport/LiveBeat/'
-    basepath = '/Users/Rich/Documents/Twitch'
-    positive_path = os.path.join(basepath, 'test_images_button')
-    negative_path = os.path.join(basepath, 'test_images_non-button')
-
     positive_list = file_list(positive_path)
     negative_list = file_list(negative_path)
     X = []
@@ -80,21 +76,46 @@ def scene_detection():
 
     return (model, pca)
 
-def segmenter(video_id, model, pca, threshold=0.5, seconds_between_frames=60):
+def get_health_status(img):
+    # Player status
+    h, w, c = img.shape
+    factor = 0.226
+    y1 = int(h * 0.044)
+    x1 = int(w * (0.5 - factor))
+    y2 = y1+1
+    x2 = int(w * (0.5 + factor))
+    pl_status = img[y1:y2, x1:x2, :]
+
+    # Block out center of player status
+    h, w, c = pl_status.shape
+    factor = .12
+    x1 = int(np.round(w * (0.5 - factor)))+1
+    x2 = int(np.round(w * (0.5 + factor)))
+    pl_status[:, x1:x2, :] = 0
+
+    img = pl_status[:,:,::-1] # Put player status from RGB to BGR for OpenCV
+    hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    hist = cv2.calcHist([hsv], [0], None, [256], [0,256])
+    hist[0] = 0 # Get rid of first element since it's no. of pixels in img
+
+    # Initialize colors
+    red = green = 0
+    red_sum = int(sum(hist[0:12]))
+    green_sum = int(sum(hist[55:65]))
+    if red_sum > 0:
+        red = red_sum
+    if green_sum > 0:
+        green = green_sum
+
+    return (red, green)
+
+def segmenter(video_path, model, pca, threshold=0.5, seconds_between_frames=60):
     """Generate timecodes for game and non-game segments"""
 
     # Print status
     print('Finding timecodes for segments')
 
     # Open file handle
-    # basepath = '/Volumes/Passport/LiveBeat/'
-    basepath = '/Users/Rich/Documents/Twitch'
-    video_path = os.path.join(
-        basepath,
-        'video',
-        'dota2ti_v{}_720p30.mp4'.format(video_id)
-    )
-
     vid = imageio.get_reader(video_path, 'ffmpeg')
 
     # Get metadata and select 1 frame every n seconds
@@ -104,7 +125,7 @@ def segmenter(video_id, model, pca, threshold=0.5, seconds_between_frames=60):
     frames = np.arange(0, nframes, seconds_between_frames*fps)
 
     # Check frames
-    timecodes = []
+    timecodes = statuses = []
     start_time = end_time = 0
 
     # Run through frames and find segments
@@ -122,19 +143,27 @@ def segmenter(video_id, model, pca, threshold=0.5, seconds_between_frames=60):
         y2 = y1 + 26
         shop = img[y1:y2, x1:x2, :]
 
+        # Get player status
+        red, green = get_health_status(img)
+
         # Generate predictions for each selected frame
         features = pca.transform(img2features(shop))
         features = np.array(features).reshape(1, -1)
         prediction = model.predict(features)
 
+        if prediction >= threshold:
+            statuses.append([i, red, green])
         if prediction >= threshold and not start_time:
-            start_time = '{}'.format(i)
+            start_time = i
         if prediction <= threshold and start_time:
-            end_time = '{}'.format(i)
-            timecodes.append(','.join([start_time, end_time]))
-            print('Found:', [start_time, end_time])
-            start_time = 0
-            end_time = 0
+            end_time = i
+            # Filter out short segments out
+            # fps (60) * step (30 sec) * n segments
+            if end_time - start_time > (60 * 30 * 6):
+                timecodes.append([start_time, end_time])
+                print('Found:', [start_time, end_time])
+                start_time = 0
+                end_time = 0
 
     # Close video handle to release thread and buffer
     vid.close()
@@ -144,18 +173,16 @@ def segmenter(video_id, model, pca, threshold=0.5, seconds_between_frames=60):
     df = pd.DataFrame(values)
     df.columns = ['game']
     for row in timecodes:
-        start, stop = (int(row[0])/fps, int(row[1])/fps)
-        # Filter out short segments of 7200 frames (4 * 60 fps * 30 sec)
-        if stop - start > 120:
-            df.loc[(df.index >= start) & (df.index < stop), 'game'] = 1
+        start, stop = (row[0]/fps, row[1]/fps)
+        df.loc[(df.index >= start) & (df.index < stop), 'game'] = 1
 
-    return df
+    return df, statuses
 
 def query_to_video(query):
     video_id = re.findall('\/(\d+)', query)[0]
     return video_id
 
-def compile_chat(v_id):
+def compile_chat(chat_path):
     def agg_chat(data):
         """Aggregate JSON chat data into a row"""
         attr = data['attributes']
@@ -182,13 +209,6 @@ def compile_chat(v_id):
         }
 
         return row
-
-    # basepath = '/Volumes/Passport/LiveBeat/'
-    basepath = '/Users/Rich/Documents/Twitch'
-    chat_path = os.path.join(basepath,
-                             'chat',
-                             'v{}'.format(v_id)
-                            )
 
     # Aggregate files into dictionary
     aggregated = []
@@ -260,25 +280,21 @@ def TEMP_HAVE_STAMPS(video_id):
     df.columns = ['game']
     for row in timecodes:
         start, stop = (row[0]/fps, row[1]/fps)
-        # Filter out short segments of 7200 frames (4 * 60 fps * 30 sec)
+        # Filter out short segments of 120 seconds (4 * 30 sec)
         if stop - start > 120:
             df.loc[(df.index >= start) & (df.index < stop), 'game'] = 1
 
     return df
 
-def build_scrub_plot(video_id, game, chat):
+def build_scrub_plot(target_path, game, chat):
     fig, ax1 = plt.subplots(figsize=(18,2))
     ax2 = ax1.twinx()
     ax1.plot(game.index, game['game'], '.b', markersize=8)
     ax2.plot(chat.index, chat['frequency'], '-r')
     ax1.set_xlim([0, game.index.max()])
 
-    basepath = '/Users/Rich/Documents/Flask/flaskexample/static/graphs'
     plt.savefig(
-        os.path.join(
-            basepath,
-            '{}.png'.format(video_id)
-        ),
+        target_path,
         bbox_inches='tight',
     )
 
@@ -289,29 +305,37 @@ def index():
 
 @app.route('/go', methods=['POST'])
 def go():
+    # Define basepath for file locations
+    # basepath = '/Volumes/Passport/LiveBeat/'
+    basepath = '/Users/Rich/Documents/Twitch'
+
     # Process query to identify video of request
     query = request.form['query']
     video_id = query_to_video(query)
+    video_file = 'dota2ti_v{}_720p30.mp4'.format(video_id)
+    video_path = os.path.join(basepath, 'video', video_file)
+    chat_path = os.path.join(basepath, 'chat', 'v{}'.format(video_id))
 
     # Get scene detector and acquire game timecodes
-    # model, pca = scene_detection()
-    # game = segmenter(video_id, model, pca)
-    game = TEMP_HAVE_STAMPS(video_id)
+    positive_path = os.path.join(basepath, 'test_images_button')
+    negative_path = os.path.join(basepath, 'test_images_non-button')
+    model, pca = scene_detection(positive_path, negative_path)
+
+    game, statuses = segmenter(video_path, model, pca)
+    # game = TEMP_HAVE_STAMPS(video_id)
 
     # Get chat data
-    chat = compile_chat(video_id)
-
-    # Build video path
-    # basepath = '/Volumes/Passport/LiveBeat/'
-    basepath = '/Users/Rich/Documents/Twitch'
-    video_path = os.path.join(
-        basepath,
-        'video',
-        'dota2ti_v{}_720p30.mp4'.format(video_id)
-    )
+    chat = compile_chat(chat_path)
 
     # Build image for scrub plot
-    build_scrub_plot(video_id, game, chat)
+    basepath = '/Users/Rich/Documents/Flask/flaskexample/static/graphs'
+    target_path = os.path.join(basepath, '{}.png'.format(video_id))
+    build_scrub_plot(target_path, game, chat)
+
+    print(statuses)
+    statuses = pd.DataFrame(statuses)
+    statuses.columns = ['frame', 'red', 'green']
+    statuses.to_csv('/Users/Rich/Documents/Twitch/statuses/test.csv')
 
     return render_template(
         'go.html',
